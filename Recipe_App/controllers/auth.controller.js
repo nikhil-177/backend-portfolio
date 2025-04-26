d2826c046bc2dc2b720c42c5752784f3fdc98fa5
nikhil-177 <pn2683249@gmail.com>
2025-04-26 17:08:25 +0530

    user login done

--- Recipe_App/controllers/auth.controller.js
+++ Recipe_App/controllers/auth.controller.js
@@ -18,3 +18,38 @@ export const registerUser = async (req, res) => {
     });
   }
 };
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (user === null) {
+      return res.status(400).json({
+        message: 'Invalid Email or Password',
+        statusCode: 400,
+        success: false,
+      });
+    }
+    // check password
+    if (!(user.password === password)) {
+      return res.status(400).json({
+        message: 'Invalid Email or Password',
+        statusCode: 400,
+        success: false,
+      });
+    } else {
+      return res.status(200).json({
+        message: `Welcome back, ${user.name}`,
+        tokens: [],
+        statusCode: 200,
+        success: true,
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message || 'Something went wrong',
+      statusCode: 500,
+      success: false,
+    });
+  }
+};
