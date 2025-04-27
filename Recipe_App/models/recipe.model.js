eac15c81115417624eb957dfb0e94bc3141e0675
nikhil-177 <pn2683249@gmail.com>
2025-04-27 14:00:16 +0530

    recipe model made

---
+++ Recipe_App/models/recipe.model.js
@@ -0,0 +1,80 @@
+import mongoose from 'mongoose';
+
+const recipeSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    ingredients: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    instructions: [
+      {
+        step: {
+          type: String,
+          required: true,
+        },
+        order: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    images: [
+      {
+        type: String,
+        required: false,
+      },
+    ],
+    preparationTime: {
+      type: Number, // Time in minutes
+      required: true,
+    },
+    cookingTime: {
+      type: Number, // Time in minutes
+      required: true,
+    },
+    servings: {
+      type: Number,
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+    comments: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Comment',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+recipeSchema.pre('save', function (next) {
+  this.updatedAt = Date.now();
+  next();
+});
+
+export const Recipe = mongoose.model('Recipe', recipeSchema);
